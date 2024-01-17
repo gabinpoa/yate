1ff71de934b0b534f17f30e56ddf336c7ec32c80
Gabriel Nascimento <gabrielnpoa@gmail.com>
2024-01-17 01:12:55 +0000

    Add visual cursor

--- main.lua
+++ main.lua
@@ -1,12 +1,16 @@
 ---@diagnostic disable: duplicate-set-field
 _G.love = require "love"
+local keymaps = require "lua.keymaps"
 local  text = {""}
 local cursor = {
     line = 1,
-    pos = 1
-  }
+    pos = 1,
+}
+local lineHeight
 
 function love.load()
+  lineHeight = love.graphics.getFont():getHeight()
+  love.keyboard.setKeyRepeat(true)
   text = { n = 4, "hey, this is the first line", "and this is the second", "this the third","table"}
   cursor.line = text.n
   cursor.pos = text[text.n]:len()
@@ -16,7 +20,10 @@ function love.update(dt)
 end
 
 function love.draw()
-  love.graphics.print(table.concat(text, "\n"))
+  love.graphics.print(table.concat(text, "\n"), 10, 10)
+  cursor.x = 10 + love.graphics.getFont():getWidth(text[cursor.line]:sub(1, cursor.pos))
+  cursor.y = 10 + (cursor.line - 1) * lineHeight
+  love.graphics.line(cursor.x, cursor.y, cursor.x, cursor.y + lineHeight)
 end
 
 function love.textinput(t)
@@ -25,4 +32,7 @@ function love.textinput(t)
 end
 
 function love.keypressed(key)
+  if keymaps[key] ~= nil then
+    keymaps[key](cursor, text)
+  end
 end
