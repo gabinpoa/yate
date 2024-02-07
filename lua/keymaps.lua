9e49bb2fcda70ad38ff4c4a132a5f04f64d3fd16
Gabriel Nascimento <gabrielnpoa@gmail.com>
2024-02-07 19:14:49 +0000

    Add remove and to end of prev line; Fix go to next line when curr ends

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,21 +1,30 @@
 return {
-    backspace = function (cursor, text)
-        if cursor.pos -1 >= 1 then
+    backspace = function(cursor, text)
+        if cursor.pos - 1 > 0 then
             cursor.pos = cursor.pos - 1
-            text[cursor.line] = text[cursor.line]:sub(1, cursor.pos) .. text[cursor.line]:sub(cursor.pos+2, text[cursor.line]:len())
-        end
-    end,
-    left = function (cursor, text)
-        if cursor.pos -1 < 1 and cursor.line -1 > 0 then
-            cursor.line = cursor.line -1
+            text[cursor.line] = text[cursor.line]:sub(1, cursor.pos) ..
+            text[cursor.line]:sub(cursor.pos + 2, text[cursor.line]:len())
+        elseif cursor.pos == 1 then
+            cursor.pos = 0
+            text[cursor.line] = ""
+        elseif cursor.pos == 0 and cursor.line - 1 > 0 then
+            table.remove(text, cursor.line)
+            text.n = text.n - 1
+            cursor.line = cursor.line - 1
             cursor.pos = text[cursor.line]:len()
-        elseif cursor.pos -1 >= 1 then
+        end
+    end,
+    left = function(cursor, text)
+        if cursor.pos - 1 < 0 and cursor.line - 1 > 0 then
+            cursor.line = cursor.line - 1
+            cursor.pos = text[cursor.line]:len()
+        elseif cursor.pos - 1 >= 0 then
             cursor.pos = cursor.pos - 1
         end
     end,
-    right = function (cursor, text)
-        if cursor.pos + 1 > text[cursor.line]:len() and cursor.line +1 <= text.n then
-            cursor.pos = 1
+    right = function(cursor, text)
+        if cursor.pos + 1 > text[cursor.line]:len() and cursor.line + 1 <= text.n then
+            cursor.pos = 0
             cursor.line = cursor.line + 1
         elseif cursor.pos + 1 <= text[cursor.line]:len() then
             cursor.pos = cursor.pos + 1
