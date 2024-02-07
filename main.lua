434dad879b43371792d84eaed4127c347862aa7a
Gabriel Nascimento <gabrielnpoa@gmail.com>
2024-02-07 19:10:40 +0000

    Formatting changes

--- main.lua
+++ main.lua
@@ -1,17 +1,17 @@
 ---@diagnostic disable: duplicate-set-field
 _G.love = require "love"
 local keymaps = require "lua.keymaps"
-local  text = {""}
+local text = { "" }
 local cursor = {
-    line = 1,
-    pos = 1,
+  line = 1,
+  pos = 1,
 }
 local lineHeight
 
 function love.load()
   lineHeight = love.graphics.getFont():getHeight()
   love.keyboard.setKeyRepeat(true)
-  text = { n = 4, "hey, this is the first line", "and this is the second", "this the third","table"}
+  text = { n = 4, "hey, this is the first line", "and this is the second", "this the third", "table" }
   cursor.line = text.n
   cursor.pos = text[text.n]:len()
 end
@@ -27,7 +27,8 @@ function love.draw()
 end
 
 function love.textinput(t)
-  text[cursor.line] = text[cursor.line]:sub(1, cursor.pos) .. t .. text[cursor.line]:sub(cursor.pos+1, text[cursor.line]:len())
+  text[cursor.line] = text[cursor.line]:sub(1, cursor.pos) ..
+  t .. text[cursor.line]:sub(cursor.pos + 1, text[cursor.line]:len())
   cursor.pos = cursor.pos + 1
 end
 
